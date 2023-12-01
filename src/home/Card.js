49cd2765d1b4dccf1985fba5e35a86e44f05740a
Jgithubacob123! <jakenaumann@yahoo.com>
2023-12-01 00:30:01 -0500

    Added:     -Card.css (moved from Homepage.css)
    Modified:
        -Common.css (smaller header changes)
        -Homepage.css (moved 'Card' css to its own file)
        -Index.css (changed some root colors)
        -Card.js (Added card-top and card-bottom)

--- src/home/Card.js
+++ src/home/Card.js
@@ -2,14 +2,19 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../assets/styles/Homepage.css'; // Make sure to create this CSS file
+import './Card.css'; 
 
 const Card = ({ title, link }) => {
   return (
     <Link to={link} className="card">
       <div className="card-content">
-        <h2 className="card-title">{title}</h2>
-        <p className="card-text">This is the text for the {title.toLowerCase()} section of the website.</p>
+        <div className="card-top">
+          <h2 className="card-title">{title}</h2>
+        </div>
+        <div className="card-bottom">
+          <p className="card-text">This is the text for the {title.toLowerCase()} section of the website.</p>
+        </div>
+        
       </div>
     </Link>
   );
